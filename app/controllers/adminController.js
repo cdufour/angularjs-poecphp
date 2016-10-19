7d463308af6121acc58e7e82630e824ddeb4453b
Opusidea <opusidea@gmail.com>
2016-10-19 14:03:33 +0200

    commit 6

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,11 +1,24 @@
 angular.module("searchApp")
-  .controller("adminController", function($scope) {
+  .controller("adminController", function($scope, $rootScope) {
 
-    $scope.client = {};
+    // déclaration facultative (JS le fera de lui-même sinon)
+    // d'un object client initialisé avec des valeurs null
+    // ces valeurs seront mises à jour par les directives ng-model
+    // attachées aux input du formulaire
+    $scope.client = {
+      name: null,
+      postcode: null,
+      country: null,
+      password: null // propriété non gérée dans la vue (pas d'input)
+    };
 
     // on ajoute au $scope la function addClient()
     $scope.addClient = function() {
-      console.log($scope.client);
+      // ajouter le client au tableau de clients
+      $rootScope.clients.push($scope.client);
+      console.log($rootScope.clients);
     };
+
+
 
   }); // fin adminController
--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,5 +1,5 @@
 angular.module("searchApp")
-  .controller("mainController", function($scope) {
+  .controller("mainController", function($scope, $rootScope) {
   // injection de dépendance
   // on fournit le service $scope en entrée de la function
   // afin que le controller puisse d'adresser à la vue
@@ -12,7 +12,27 @@ angular.module("searchApp")
   {name:'Kautzmann', postcode:67000, password:456, country:'Italie'}];
 
   // passage des clients à la vue via le scope
-  $scope.clients = data;
+  //$scope.clients = data;
+  // variante : on place les données au niveau du rootScope
+  // afin que d'autres controllers aient accès à ces données
+
+  if ($rootScope.clients) {
+    // si $rootScope.clients existe, s'il n'est pas Undefined
+    //if ($rootScope.clients.length != data.length) {
+      // la longueur des tableaux diffère, conclusion:
+      // des clients ont été ajoutés (ou supprimés) via
+      // l'interface d'administration, qui offre
+      // justement des possibilités d'agir sur $rootScope.clients
+    //}
+  } else {
+    // si $rootScope.data n'existe pas,
+    // on le crée (en lui donnant une valeur initiale)
+    // nous avons résolu le problème de l'écrasement
+    // systématique de $rootScope.clients par le tableau initial
+    $rootScope.clients = data;
+  }
+
+
   // initialisation des variables
   $scope.criterium = 'name';
   $scope.reverse = false; // A-Z
